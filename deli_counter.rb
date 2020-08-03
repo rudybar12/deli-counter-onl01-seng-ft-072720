159843b26be555bf1fe145955c965e31a00d4a9e
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-08-03 01:32:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  elsif
+      puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+    end
+end
+
+#def take_a_number()
