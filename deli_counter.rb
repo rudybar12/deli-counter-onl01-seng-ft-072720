3e00714caf3e6781e2863395717a2dea1ce1e0f5
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-08-03 01:56:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,13 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts " There is nobody waiting to b served!"
+  else
+    serving = katz_deli[0]
+    puts "Currently serving #{serving}."
+    katz_deli.shift
+  end
+end 
